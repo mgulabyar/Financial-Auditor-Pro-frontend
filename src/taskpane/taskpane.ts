460dab7d19151ebac21e7f59f62a470447158295
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-06-23 16:47:50 +0500

    feat: configure taskpane.ts entry logic for office application initializing

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -1,7 +1,6 @@
 /* global Excel console */
 
 export async function insertText(text: string) {
-  // Write text to the top left cell.
   try {
     await Excel.run(async (context) => {
       const sheet = context.workbook.worksheets.getActiveWorksheet();
